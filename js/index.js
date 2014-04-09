3f7012e1720c4f5836735a2fa6521e76dd0bcb58
Carlos Gómez Cainzos <carlos.gomezcainzos@gmail.com>
2014-04-09 02:10:23 +0200

    investigando enviar notificaciones

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(function() {
    if (online){
    	var token = localStorage.endpoint || null;
    	$('#AppToken').html('Mi AppToken: ' + token);
-   	$('#AppToken').html('Mi AppToken: ' + localStorage.endpoint);
+   	//$('#AppToken').html('Mi AppToken: ' + localStorage.endpoint);
    }
    else{
    	alert('Conecte el dispositivo a una red con datos');
@@ -27,6 +27,42 @@ function timeOut_init() {
 document.querySelector('#pushbtn').addEventListener ('click', function(){
 	alert('Sent new version to push notification server')
 })
+
+
+//Usar JSON.stringify({x: 5, y: 6}); 
+function notify(myEndpoint, msg, issuing) {
+    			request.put({
+    				url: myEndpoint,
+    				body: "version=" + new Date().getTime()
+    			}, function (error, response, body) {
+    				if(response && response.statusCode == 200){
+    					var time = new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString();
+    					console.log(body);
+    					console.log(time + ' NOTIFICACION ENVIADA --> EMISOR: ' + issuing + ' MENSAJE: ' + msg);
+    				} else {
+    					console.log(error);
+    					console.log(body);
+    				}
+    			}
+    	}
+function updateContact (contact) {
+
+     jQuery.ajax({
+         type: "PUT",
+         url: "http://localhost:49193/Contacts.svc/Update",
+         contentType: "application/json; charset=utf-8",
+         data: contact.toJsonString(),
+         dataType: "json",
+         success: function (data, status, jqXHR) {
+             // do something
+         },
+     
+         error: function (jqXHR, status) {
+             // error handler
+     
+         }     
+     });     
+}
 /*document.querySelector('#hello').addEventListener ('click', function () {
 
 alert('hacer hello');
